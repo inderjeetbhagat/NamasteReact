843cce887e3d64fbdbc5b758bec3ba091ad4e7f2
inderjeetbhagat <62701663+inderjeetbhagat@users.noreply.github.com>
2024-05-12 11:08:13 +0530

    Categoriezed into components

--- IndeFood/App.js
+++ IndeFood/App.js
@@ -1,1 +1,24 @@
-const React = require("react")
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div>
+                <img src="https://png.pngtree.com/template/20191014/ourmid/pngtree-pin-food-delivery-map-location-delivery-logo-concept-image_318151.jpg"></img>
+            </div>
+
+        </div>
+    )
+}
+
+
+const AppLayout = () => {
+    return(
+        <div className="app">
+            //Header
+
+
+        </div>
+    )
+}
